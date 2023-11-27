ddbc66657eb9e5f489374cb51f954c200e2a4c2e
bongj9 <lollypark501@naver.com>
2023-11-27 19:21:58 +0900

    feat:Member생성

---
+++ src/main/java/jpabook/jpashop/Member.java
@@ -0,0 +1,19 @@
+package jpabook.jpashop;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id @GeneratedValue //jpa를 생성할때 쓰는것
+    private Long id;
+    private Long username;
+
+
+
+}
